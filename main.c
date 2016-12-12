b56fd76386e5e1526de358f13a03cacc410edaa0
Edward Higgins <Edward.Higgins21@gmail.com>
2016-12-12 12:20:30 +0000

    Simple multithreading test

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <process.h>
 #include "socket.h"
+
+int handle_connection(SOCKET c_sock, char* message) {
+    while(1) {
+            s_recv(c_sock, message);
+            printf("%s", message);
+            memset(message, 0, sizeof(message));
+    }
+    return 0;
+}
 
 int main()
 {
     SOCKET sock = server_setup();
+    SOCKET socklist[2] =
     char message[513] = {0};
 
     while(1) {
@@ -11,11 +22,8 @@ int main()
         SOCKET c_sock = s_accept(sock);
         s_send(c_sock);
 
-        while(1) {
-            s_recv(c_sock, message);
-            printf("%s", message);
-            memset(message, 0, sizeof(message));
-        }
+        _beginthread(handle_connection(c_sock, message), 0, NULL);
+
         printf("Connection closed\n");
 
         closesocket(c_sock);

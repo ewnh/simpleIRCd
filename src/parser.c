725f4a419e417351c87e999d22ac845ab7453b8a
Edward Higgins <Edward.Higgins21@gmail.com>
2016-12-28 21:05:43 +0000

    Added custom version of strtok_r

--- src/parser.c
+++ src/parser.c
@@ -13,6 +13,41 @@
 
 struct channel* channels = NULL;
 
+/*
+ * public domain strtok_r() by Charlie Gordon
+ *
+ *   from comp.lang.c  9/14/2007
+ *
+ *      http://groups.google.com/group/comp.lang.c/msg/2ab1ecbb86646684
+ *
+ *     (Declaration that it's public domain):
+ *      http://groups.google.com/group/comp.lang.c/msg/7c7b39328fefab9c
+ */
+
+//Unfortunately, MinGW does not contain an implementation of strtok_r
+
+char* strtok_r(char *str, const char *delim, char **nextp) {
+    char *ret;
+
+    if (str == NULL) {
+        str = *nextp;
+    }
+
+    str += strspn(str, delim);
+    if (*str == '\0') {
+        return NULL;
+    }
+
+    ret = str;
+    str += strcspn(str, delim);
+    if (*str) {
+        *str++ = '\0';
+    }
+
+    *nextp = str;
+    return ret;
+}
+
 void handle_connection(struct user* hc) {
     while(1) {
         memset(hc->message, 0, 513);
@@ -26,9 +61,9 @@ void handle_connection(struct user* hc) {
             continue;
         }
 
-        char* strptr = strtok(hc->message, " ");
-        if(strcmp(strptr, "JOIN") == 0) {
-            join_channel(&channels, hc, strtok(NULL, " "));
+        char* strptr;
+        if(strcmp(strtok_r(hc->message, " ", &strptr), "JOIN") == 0) {
+            join_channel(&channels, hc, strtok_r(NULL, " ", &strptr));
         }
     }
     printf("Connection closed\n");
